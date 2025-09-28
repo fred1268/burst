b176634d07a71f70cabfcb28be2f1904c0ddc111
Fred <frederic.thomas@live.com>
2025-09-28 15:19:53 +0200

    implement the database upgrade process for migrations

--- src/tools/db.rs
+++ src/tools/db.rs
@@ -1,6 +1,7 @@
 use crate::cmds::constants::BURST_METADATA_FILE;
 use crate::tools::cmderror::CmdError::{self, DbError};
 use crate::tools::fs::FileSystem;
+use crate::tools::version::VERSION_1_1;
 use rusqlite::{Connection, Params, Result, Row, params};
 use rusqlite_regex;
 use std::path::Path;
@@ -16,6 +17,13 @@ const SS_ZERO: &str = "INSERT INTO snapshots (id) VALUES (0)";
 const FV_TABLE: &str = "CREATE TABLE fileversions (id INTEGER PRIMARY KEY, digest TEXT DEFAULT '', path TEXT, archive TEXT, name TEXT, size INTEGER, compressed_size INTEGER DEFAULT 0, encrypted BOOLEAN DEFAULT false, created DATETIME, modified DATETIME, deleted_sid INTEGER, is_dir INTEGER DEFAULT 0, FOREIGN KEY (deleted_sid) REFERENCES snapshots(id))";
 
 const SF_TABLE: &str = "CREATE TABLE snapshotfiles (snapshot_id INTEGER, version_id INTEGER, PRIMARY KEY (snapshot_id, version_id), FOREIGN KEY (snapshot_id) REFERENCES snapshots(id), FOREIGN KEY (version_id) REFERENCES fileversions(id))";
+
+const FH_TABLE: &str = "CREATE TABLE filehistory (id INTEGER, snapshot_id INTEGER, digest TEXT DEFAULT '', path TEXT, archive TEXT, name TEXT, size INTEGER, compressed_size INTEGER DEFAULT 0, encrypted BOOLEAN DEFAULT false, created DATETIME, modified DATETIME, deleted_sid INTEGER, is_dir INTEGER DEFAULT 0, FOREIGN KEY (deleted_sid) REFERENCES snapshots(id), FOREIGN KEY (snapshot_id) REFERENCES snapshots(id))";
+
+const VER_TABLE: &str = "CREATE TABLE version (id TEXT)";
+const VER_ZERO: &str = "INSERT INTO version (id) VALUES ('1.0')";
+const VER_UPDATE: &str = "UPDATE version SET id=?1";
+const VER_READ: &str = "SELECT id FROM version";
 
 const INDEX_FV: &str = "CREATE INDEX idx_fileversions ON fileversions(path, name)";
 const INDEX_SF1: &str = "CREATE INDEX idx_snapshotfiles_snapshot ON snapshotfiles(snapshot_id)";
@@ -37,6 +45,7 @@ impl Database {
             connection: Connection::open(&db_file).map_err(|err| DbError(String::from("Cannot open database"), err.to_string()))?,
         };
         db.check_tables()?;
+        db.upgrade()?;
         Ok(db)
     }
 
@@ -60,6 +69,10 @@ impl Database {
             self.execute(SF_TABLE, ())?;
             self.execute(INDEX_SF1, ())?;
             self.execute(INDEX_SF2, ())?;
+        }
+        if !self.exists(TABLE_EXISTS, params!["version"])? {
+            self.execute(VER_TABLE, ())?;
+            self.execute(VER_ZERO, ())?;
         }
         Ok(())
     }
@@ -109,5 +122,34 @@ impl Database {
             result.push(one);
         }
         Ok(result)
+    }
+
+    fn upgrade(&self) -> Result<(), CmdError> {
+        loop {
+            let v: Option<String> = self.query_one(VER_READ, params![], |row| row.get(0))?;
+            let version = match v {
+                Some(v) => v,
+                None => return Err(DbError(String::from("Version"), String::from("Not found"))),
+            };
+            match version.as_str() {
+                "1.0" => self.upgrade_to_1_1()?,
+                "x.y" => self.upgrade_to_x_y()?,
+                _ => break,
+            };
+        }
+        Ok(())
+    }
+
+    fn upgrade_to_1_1(&self) -> Result<(), CmdError> {
+        self.execute(FH_TABLE, ())?;
+        self.execute("INSERT INTO filehistory SELECT id, snapshot_id, digest, path, archive, name, size, compressed_size, encrypted, created, modified, deleted_sid, is_dir FROM fileversions fv JOIN snapshotfiles sf ON fv.id=sf.snapshot_id WHERE sf.snapshot_id IN (SELECT id FROM snapshots ORDER BY created DESC LIMIT 1)", ())?;
+        self.execute(VER_UPDATE, params![VERSION_1_1])
+    }
+
+    fn upgrade_to_x_y(&self) -> Result<(), CmdError> {
+        // upgrade code here
+
+        // self.execute(VER_UPDATE, params![VERSION_x_y])
+        Ok(())
     }
 }
--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -2,3 +2,4 @@ pub mod cmderror;
 pub mod db;
 pub mod fmt;
 pub mod fs;
+pub mod version;
