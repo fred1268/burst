72939267027a0a5ecdc1ea78386e381549e11066
Fred <frederic.thomas@live.com>
2026-05-13 15:25:48 +0200

    more linear code

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,25 @@ use std::{env, process::exit};
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    match command::get_command(&args).await {
-        Ok(mut c) => match c.validate() {
-            Ok(_) => match c.run().await {
-                Ok(_) => (),
-                Err(err) => {
-                    eprintln!("{}\n", err);
-                    exit(1);
-                }
-            },
-            Err(err) => {
-                eprintln!("{}\n", err);
-                c.help();
-                exit(2);
-            }
-        },
-        Err(err) => match err {
-            MissingCommand => {
-                eprintln!("{}\n", err);
-                HelpCommand::default_help(&args[0]);
-                exit(0);
-            }
-            _ => {
-                eprintln!("{}\n", err);
-                exit(1);
-            }
-        },
+    let mut cmd = command::get_command(&args).await.unwrap_or_else(|err| match err {
+        MissingCommand => {
+            eprintln!("{}\n", err);
+            HelpCommand::default_help(&args[0]);
+            exit(0);
+        }
+        _ => {
+            eprintln!("{}\n", err);
+            exit(1);
+        }
+    });
+    if let Err(err) = cmd.validate() {
+        eprintln!("{}\n", err);
+        cmd.help();
+        exit(2);
+    }
+    if let Err(err) = cmd.run().await {
+        eprintln!("{}\n", err);
+        exit(1);
     }
     exit(0);
 }
